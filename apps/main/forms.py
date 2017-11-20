fa4edddfceda69dea75dac4028000f7dc1c9d2d0
李利锋 <lilifeng@meican-inc.com>
2017-11-20 14:49:24 +0800

    login

---
+++ apps/main/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from apps.core import photos
+from wtforms import SubmitField
+
+class MyForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    passowrd = StringField('passowrd', validators=[DataRequired()])
+
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[
+        FileAllowed(photos, u'只能上传图片！'),
+        FileRequired(u'文件未选择！')])
+    submit = SubmitField(u'上传')
--- apps/main/view.py
+++ apps/main/view.py
@@ -1,9 +1,21 @@
 from flask import Blueprint, render_template, session, request,jsonify
 import time
 from apps.main.models import *
+from .forms import MyForm
+from apps.core import db
 
 mod = Blueprint('socketdemo', __name__, url_prefix='/', template_folder='templates')
 
 @mod.route('/', methods=("post", "get"))
 def index():
     return render_template('index.html')
+
+@mod.route('/login' ,methods=("post", "get"))
+def login():
+    form = MyForm()
+    if not form.validate_on_submit():
+        return render_template('login.html', form=form)
+    user = db.session.query(User).filter_by(username=form.username,password=form.password).first()
+    if user == None:
+        return render_template('login.html', form=form)
+    return '登陆成功'
